faeb59b560af69593429cddfc610e138551d11c2
Harri <harri.kinsman@gmail.com>
2023-05-29 20:20:41 +0100

    Style the todo app

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const taskInput = document.getElementById("taskInput");
 const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
+const removeButton = document.getElementById("removeButton");
 
 addButton.addEventListener("click", addTask);
+document.addEventListener("keydown", event => {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
 
 function addTask() {
   const taskText = taskInput.value;
-  if (taskText !== "") {
-    const li = document.createElement("li");
-    li.innerText = taskText;
-    taskList.appendChild(li);
+  if (taskText.trim() !== "") {
+    const task = document.createElement("li");
+    task.className = "task";
+    task.innerHTML = `
+      <span class="task-text">${taskText}</span>
+      <button class="task-remove">x</button>
+    `;
+    taskList.appendChild(task);
     taskInput.value = "";
+
+    const removeButton = task.querySelector(".task-remove");
+    removeButton.addEventListener("click", () => {
+      task.remove();
+    });
   }
 }
