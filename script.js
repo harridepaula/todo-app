cc4a88e428fdaa63558cbe71f08f9b24527eae03
Harri <harri.kinsman@gmail.com>
2023-05-30 08:57:51 +0100

    Add drag and drop feature

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function addTask() {
   if (taskText.trim() !== "") {
     const task = document.createElement("li");
     task.className = "task";
+    task.draggable = true; // Make the task element draggable
     task.innerHTML = `
       <span class="task-text">${taskText}</span>
       <button class="task-remove">x</button>
@@ -26,5 +27,51 @@ function addTask() {
     removeButton.addEventListener("click", () => {
       task.remove();
     });
+
+    // Add event listeners for drag and drop functionality
+    task.addEventListener("dragstart", handleDragStart);
+    task.addEventListener("dragover", handleDragOver);
+    task.addEventListener("dragenter", handleDragEnter);
+    task.addEventListener("dragleave", handleDragLeave);
+    task.addEventListener("drop", handleDrop);
+    task.addEventListener("dragend", handleDragEnd);
   }
+}
+
+let dragTask = null;
+
+function handleDragStart(event) {
+dragTask = this;
+event.dataTransfer.effectAllowed = "move";
+event.dataTransfer.setData("text/html", this.innerHTML);
+this.classList.add("dragging");
+}
+
+function handleDragOver(event) {
+event.preventDefault();
+event.dataTransfer.dropEffect = "move";
+}
+
+function handleDragEnter(event) {
+this.classList.add("over");
+}
+
+function handleDragLeave(event) {
+this.classList.remove("over");
+}
+
+function handleDrop(event) {
+event.preventDefault();
+if (dragTask !== this) {
+  // Remove the dragged task from its original position
+  dragTask.parentNode.removeChild(dragTask);
+  // Get the drop index of the current task
+  const dropIndex = Array.from(taskList.children).indexOf(this);
+  // Insert the dragged task at the new drop position
+  taskList.insertBefore(dragTask, this);
+}
+}
+
+function handleDragEnd(event) {
+this.classList.remove("dragging");
 }
